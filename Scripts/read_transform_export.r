99ad1ecb0aaa1b84a68ddb4aca5bf16a38e56bba
Betz <BBETZ@USAID.GOV>
2022-03-30 18:28:57 -0700

    updating

--- Scripts/read_transform_export.r
+++ Scripts/read_transform_export.r
@@ -9,6 +9,22 @@ df <- read_xlsx(xlfile) %>% select(-starts_with("ach_"), -positivity, -prep_link
   mutate(across(where(is.numeric), round, 0)) %>%
 glimpse()
 
+
+results <- df %>% select(-starts_with("target_")) %>% 
+  pivot_longer(hiv_tests:prep_initiations, names_to = "indicator", values_to = "results") %>%
+  # mutate(indicator = recode(indicator, "target_prep" = "PrEP initiations",
+  #                           "target_test" = "HIV tests",
+  #                           "target_test_pos" = "Positive HIV tests",
+  #                           "target_treatment" = "Treatment initiations")) %>%
+  group_by(year, gender, region, indicator) %>% summarise(results = sum(results)) %>%
+  glimpse()
+
+
+results %>% ggplot2::ggplot(aes(x = indicator, y = results, fill=indicator)) + geom_col() + facet_grid(region ~ year)
+
+
+
+
 targets <- df %>% select(-gender, -hiv_tests, -positive_test_results, -negative_test_results, -prep_initiations, -treatment_initiations) %>% 
   pivot_longer(contains("target"), names_to = "indicator", values_to = "targets") %>%
   mutate(indicator = recode(indicator, "target_prep" = "PrEP initiations",
@@ -19,7 +35,7 @@ targets <- df %>% select(-gender, -hiv_tests, -positive_test_results, -negative_
   group_by(year, region, indicator) %>% summarise(targets = sum(targets)) %>%
   glimpse()
 
-table(targets$indicator, targets$year)
+table(targets$indicator, targets$region)
 targets %>% ggplot2::ggplot(aes(x = indicator, y = targets, fill=indicator)) + geom_col() + facet_grid(region ~ year)
 
 help("facet_grid")
