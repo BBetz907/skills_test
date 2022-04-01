ed40cbd4db1d552998f64b7cfd5962af98d06074
Betz <BBETZ@USAID.GOV>
2022-03-31 17:21:32 -0700

    update

--- Scripts/read_transform_export.r
+++ Scripts/read_transform_export.r
@@ -17,7 +17,9 @@ results <- df %>% select(-starts_with("target_")) %>%
   #                           "target_test_pos" = "Positive HIV tests",
   #                           "target_treatment" = "Treatment initiations")) %>%
   group_by(year, gender, region, indicator) %>% summarise(results = sum(results)) %>%
+  pivot_wider() %>%
   glimpse()
+
 
 
 results %>% ggplot2::ggplot(aes(x = indicator, y = results, fill=indicator)) + geom_col() + facet_grid(region ~ year)
@@ -27,10 +29,10 @@ results %>% ggplot2::ggplot(aes(x = indicator, y = results, fill=indicator)) + g
 
 targets <- df %>% select(-gender, -hiv_tests, -positive_test_results, -negative_test_results, -prep_initiations, -treatment_initiations) %>% 
   pivot_longer(contains("target"), names_to = "indicator", values_to = "targets") %>%
-  mutate(indicator = recode(indicator, "target_prep" = "PrEP initiations",
-                            "target_test" = "HIV tests",
-                            "target_test_pos" = "Positive HIV tests",
-                            "target_treatment" = "Treatment initiations"),
+  mutate(indicator = recode(indicator, "target_prep" = "PrEP_initiations",
+                            "target_test" = "HIV_tests",
+                            "target_test_pos" = "Positive_HIV_tests",
+                            "target_treatment" = "Treatment_initiations"),
          region = paste0(region, " Region")) %>%
   group_by(year, region, indicator) %>% summarise(targets = sum(targets)) %>%
   glimpse()
